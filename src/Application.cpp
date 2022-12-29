ba2ef8ec186e0ed2e51b55f1847c621c47c90340
Nam <nam.nguyen@getcerebral.com>
2022-12-30 04:30:06 +0700

    First draft commit feature_review

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,21 +1,16 @@
-#include <iostream>
 #include "Application.h"
-
+#include <iostream>
 
 void Application::init_database() {
-
 }
 
 void Application::load_database() {
-
 }
 
 void Application::save_database() {
-
 }
 
 void Application::reset_database() {
-
 }
 
 bool Application::login(Account account) {
@@ -23,15 +18,12 @@ bool Application::login(Account account) {
 }
 
 void Application::guest_menu() {
-
 }
 
 void Application::member_menu() {
-
 }
 
 void Application::admin_menu() {
-
 }
 
 bool Application::register_member() {
@@ -39,9 +31,7 @@ bool Application::register_member() {
 }
 
 void Application::unregister_member(Member member) {
-
 }
-
 
 Application::Application() {
     this->quit = false;
@@ -55,14 +45,21 @@ Application::Application(const std::string &database_path) {
     this->login_type = GUEST;
 }
 
-
 void Application::main_loop() {
-    std::cout << "EEET2482/COSC2082 ASSIGNMENT" << "\n";
-    std::cout << "VACATION HOUSE EXCHANGE APPLICATION" << "\n\n";
-    std::cout << "Instructors: Mr. Linh Tran & Phong Ngo" << "\n";
-    std::cout << "Group: Group 9" << "\n";
-    std::cout << "s3963207, Do Le long An" << "\n";
-    std::cout << "s3963207, Do Le long An" << "\n";
-    std::cout << "s3963207, Do Le long An" << "\n";
-    std::cout << "s3963207, Do Le long An" << "\n\n\n";
+    std::cout << "EEET2482/COSC2082 ASSIGNMENT"
+              << "\n";
+    std::cout << "VACATION HOUSE EXCHANGE APPLICATION"
+              << "\n\n";
+    std::cout << "Instructors: Mr. Linh Tran & Phong Ngo"
+              << "\n";
+    std::cout << "Group: Group 9"
+              << "\n";
+    std::cout << "s3963207, Do Le long An"
+              << "\n";
+    std::cout << "s3963207, Do Le long An"
+              << "\n";
+    std::cout << "s3963207, Do Le long An"
+              << "\n";
+    std::cout << "s3963207, Do Le long An"
+              << "\n\n\n";
 }
---
+++ src/entities/HouseReview.h
@@ -0,0 +1,22 @@
+#ifndef RMIT2022C_EEET2482_LODGING_APP_SRC_ENTITIES_HOUSEREVIEW_H_
+#define RMIT2022C_EEET2482_LODGING_APP_SRC_ENTITIES_HOUSEREVIEW_H_
+
+#include "House.h"
+#include "Member.h"
+#include <iostream>
+
+class HouseReview {
+  private:
+    std::string id;
+    std::string comment;
+    int rating;
+    Member member;
+    House house;
+
+  public:
+    HouseReview();
+    HouseReview(std::string id, int rating, std::string comment);
+    void show_house_review();
+};
+
+#endif//RMIT2022C_EEET2482_LODGING_APP_SRC_ENTITIES_HOUSEREVIEW_H_
