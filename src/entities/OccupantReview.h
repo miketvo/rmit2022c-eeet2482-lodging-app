e8fbb41baedb5dbaa868e027b45dd36cf1ade0fb
Nam Nguyen <namnguyen20999@gmail.com>
2022-12-31 02:29:20 +0700

    Adding namespace to header file

--- src/entities/OccupantReview.h
+++ src/entities/OccupantReview.h
@@ -2,17 +2,22 @@
 #define RMIT2022C_EEET2482_LODGING_APP_SRC_OCCUPANT_REVIEW_H_
 
 #include <iostream>
+#include "Member.h"
 
-class OccupantReview {
-  private:
-    std::string id;
-    int rating;
-    std::string comment;
+namespace OccupantReview {
+  class OccupantReview {
+    private:
+      std::string id;
+      int rating;
+      std::string comment;
+      Member member;
 
-  public:
-    OccupantReview();
-    OccupantReview(std::string id, int rating, std::string comment);
-    void show_occupant_review();
+    public:
+      OccupantReview();
+      OccupantReview(std::string id, int rating, std::string comment);
+      void show_occupant_review();
 };
+}
+
 
 #endif//RMIT2022C_EEET2482_LODGING_APP_SRC_OCCUPANT_REVIEW_H_
